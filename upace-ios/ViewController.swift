6b7b58d3c51b59276eff5613fe43228efb225304
qualwebs0410 <rathorep.qualwebs@gmail.com>
2024-03-06 12:57:45 +0530

    Initial Commit

---
+++ upace-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  upace-ios
+//
+//  Created by Qualwebs on 06/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
